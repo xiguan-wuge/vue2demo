0a82edc015ed6346afd8b12fcf93b1413c884929
xiguan-wuge <1584077483@qq.com>
2023-04-22 01:44:11 +0800

    feat: 引入vant

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Area} from 'vant'
+
+Vue.use(Area)
+// Vue.use(Picker)s
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
